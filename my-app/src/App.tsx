a79312cccc5ff9c6a56cbc62636bd571ffb93fa5
Francisco Monteiro <fmonteiro@sentilant.com>
2023-03-08 19:34:52 +0000

    Initial setup

---
+++ my-app/src/App.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./App.css";
+
+const puzzle = [
+  [1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1],
+];
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3>Sudoku solver</h3>
+        <input className="cell"></input>
+      </header>
+    </div>
+  );
+}
+
+export default App;
